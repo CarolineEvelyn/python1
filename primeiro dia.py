96027c4ff1ce6e33f53104c05404f39d0a3fdce2
CarolineEvelyn <carolineevelyn08aguiar@gmail.com>
2024-08-13 21:04:16 -0300

    Add files via upload

---
+++ primeiro dia.py
@@ -0,0 +1,9 @@
+print('ola mundo')
+
+# tipos de dados primitivos
+# INTEIRO: ... -4 -3 -2 -1 0 1 2 3 4 5 ...
+# FLOAT: 3.14 10.0 16.32 32.50 
+# STRING: alfanumérico (abcdefgh...z 0-9.,!?)
+#       Ex.: 'Davi', 'ABC', 'Caroline'
+#           'O rato roeu a roupa do rei de roma!'
+#           'Minha idade é: 15!'
---
+++ primeiro dia.py variaveis.py
@@ -0,0 +1,12 @@
+
+x = 8
+
+print(x)
+
+idade = 15
+
+pi = 3.14
+
+altura = 1.52
+
+nome = 'caroline'
